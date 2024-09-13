b73d17df6f7013f0164e9f82e293d29b17e15463
Diego Ortiz <ortizdiego84@gmail.com>
2024-09-12 23:15:50 -0300

    logo y favicon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import CartWidget from './CartWidget';
+import logo from '../assets/logo.png';
 
 const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">Tienda Dental AG</Navbar.Brand>
+      <Navbar.Brand href="#home">
+      <img
+          src={logo}
+          alt="Tienda Dental"
+          style={{ width: '150px', height: 'auto' }} 
+        />
+      </Navbar.Brand>
       <Nav className="mr-auto">
         <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#empresa">Empresa</Nav.Link>
+        <Nav.Link href="#empresa">Nosotros</Nav.Link>
         <Nav.Link href="#productos">Productos</Nav.Link>
         <Nav.Link href="#noticias">Noticias</Nav.Link>
         <Nav.Link href="#contacto">Contacto</Nav.Link>
