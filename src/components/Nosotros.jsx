2443e8d5d3daeb0b94c600eced071f427f0073a6
Ortiz Diego <ortizdiego84@gmail.com>
2024-10-25 20:00:00 -0300

    sonrrisas

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -4,6 +4,7 @@ const Nosotros = () => {
     return (
         <div>
             <h1>Sobre Nosotros</h1>
+            <h2>Inspiramos Sonrrisas</h2>
             <p>Somos una empresa dedicada a la venta de productos dentales de alta calidad.</p>
         </div>
     );
