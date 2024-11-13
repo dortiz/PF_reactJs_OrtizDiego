05407caae4e99ea513a088d306bc13b41ff4de0e
Ortiz Diego <ortizdiego84@gmail.com>
2024-11-12 23:27:28 -0300

    img varios

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
         <div>
             <h1>Bienvenido a nuestra tienda</h1>
             <p>Explora nuestros productos y servicios.</p>
-            <img src="./assets/home.jpg"  alt="imagen de la tienda" />
+            <img src="https://scontent.fcor11-2.fna.fbcdn.net/v/t1.6435-9/67491515_836734790045453_4937639947038359552_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeHy0Qr9_3k6ZEJYdJZZsDfJ27s-zDhCy5fbuz7MOELLl56zoPpzFL7SY4t3s7-gFNo&_nc_ohc=98w-U4M72CEQ7kNvgGTjbEx&_nc_zt=23&_nc_ht=scontent.fcor11-2.fna&_nc_gid=A2BiOphI9TT3EevjrPPLrTu&oh=00_AYDmj-Yl-1yYlk_-mH8DmzZTfOIXQ0X2v2aQq9NDQsfBqw&oe=675B7568"  alt="imagen de la tienda" />
         </div>
     );
 };
